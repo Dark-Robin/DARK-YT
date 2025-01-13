f6aec26ae74cd9840cbab81311552a6b0522676a
ROBIN-MAX-YT <robimmaxyt0000@gmail.com>
2025-01-13 14:30:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "Enter your session ID",
-  MONGODB: process.env.MONGODB || "Enter your mongoDB public URL",
-  OWNER_NUM: process.env.OWNER_NUM || "9470xxxxxxx",
+  SESSION_ID: process.env.SESSION_ID || "auBB2TIZ#diVrubNzGgufcR1BXeHQWnKPHFK4DdcOXVwy_Tur6BQ",
+  MONGODB: process.env.MONGODB || "mongodb://mongo:pOFewcHlYXEBBgoOutIxGoXyvoKgBeNs@autorack.proxy.rlwy.net:28275",
+  OWNER_NUM: process.env.OWNER_NUM || "94705900209",
 };
