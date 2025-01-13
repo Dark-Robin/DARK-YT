1f983c27277157ef6bba401f8359081473f944ce
ROBIN-MAX-YT <robimmaxyt0000@gmail.com>
2025-01-13 16:08:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,6 +7,6 @@ function convertToBool(text, fault = "true") {
 }
 module.exports = {
   SESSION_ID: process.env.SESSION_ID || "OnYWgb6A#Yd_e89Vhtz7N0BcnlSGhN_MN1tG7mO8jhR-CeuDbY38",
-  MONGODB: process.env.MONGODB || "mongodb://mongo:pOFewcHlYXEBBgoOutIxGoXyvoKgBeNs@autorack.proxy.rlwy.net:28275",
+  MONGODB: process.env.MONGODB || "mongodb://mongo:QfpiXGuAfauZGAIcOVcclWHAyvoqZPFX@junction.proxy.rlwy.net:32111",
   OWNER_NUM: process.env.OWNER_NUM || "94705900209",
 };
