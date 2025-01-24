ebed17cc8ca7bec8051efde98ffd6cccaee8fae9
ROBIN-MAX-YT <robimmaxyt0000@gmail.com>
2025-01-24 18:51:48 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,4 +9,8 @@ module.exports = {
   SESSION_ID: process.env.SESSION_ID || "f6owwbqL#Ko9eWfI9iglltHZV1HkIoCFg_Bz0b9oeNzA3oWqE0RY",
   OWNER_NUM: process.env.OWNER_NUM || "94705900209",
   PREFIX: process.env.PREFIX || ".",
+  ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/Dark-Robin/Bot-Helper/refs/heads/main/autoimage/Bot%20robin%20iz%20alive.jpg",
+  ALIVE_MSG: process.env.ALIVE_MSG || "Hellow I am alive now\n\n> ROBIN ALIVE MSG",
+  AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+  MODE: process.env.MODE || "public",
 };
