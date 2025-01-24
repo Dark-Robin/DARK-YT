6ab24546d24a64f7388eb99043a021b93677af93
ROBIN-MAX-YT <robimmaxyt0000@gmail.com>
2025-01-24 18:35:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "W6Z1nAzB#O3UzhRqWsrtbYR-YHEribdBBjP5S0y78rxgMrcS6OYA",
+  SESSION_ID: process.env.SESSION_ID || "f6owwbqL#Ko9eWfI9iglltHZV1HkIoCFg_Bz0b9oeNzA3oWqE0RY",
   MONGODB: process.env.MONGODB || "mongodb://mongo:WfzicwSgRYvnAvMvlAQzIAKxPLqiDIpX@junction.proxy.rlwy.net:38497",
   OWNER_NUM: process.env.OWNER_NUM || "94705900209",
 };
