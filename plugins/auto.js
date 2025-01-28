a2fd33fdf0082cc6f55934b9b1bc3f0df27558a2
Dark-Robin <sihileld23@gmail.com>
2025-01-28 14:01:13 +0530

    Update auto.js

--- plugins/auto.js
+++ plugins/auto.js
@@ -8,11 +8,11 @@ cmd({
   on: "body"
 },    
 async (robin, mek, m, { from, body, isOwner }) => {
-    const filePath = path.join(__dirname, '../my_data/autovoice.json');
+    const filePath = path.join(__dirname, '../data/autovoice.json');
     const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
     for (const text in data) {
         if (body.toLowerCase() === text.toLowerCase()) {
-            const config = await readEnv();
+          
             if (config.AUTO_VOICE === 'true') {
                 //if (isOwner) return;        
                 await robin.sendPresenceUpdate('recording', from);
@@ -27,11 +27,11 @@ cmd({
   on: "body"
 },    
 async (robin, mek, m, { from, body, isOwner }) => {
-    const filePath = path.join(__dirname, '../my_data/autosticker.json');
+    const filePath = path.join(__dirname, '../data/autosticker.json');
     const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
     for (const text in data) {
         if (body.toLowerCase() === text.toLowerCase()) {
-            const config = await readEnv();
+            
             if (config.AUTO_STICKER === 'true') {
                 //if (isOwner) return;        
                 await robin.sendMessage(from,{sticker: { url : data[text]},package: 'S_I_H_I_L_E_L'},{ quoted: mek })   
@@ -46,11 +46,11 @@ cmd({
   on: "body"
 },    
 async (robin, mek, m, { from, body, isOwner }) => {
-    const filePath = path.join(__dirname, '../my_data/autoreply.json');
+    const filePath = path.join(__dirname, '../data/autoreply.json');
     const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
     for (const text in data) {
         if (body.toLowerCase() === text.toLowerCase()) {
-            const config = await readEnv();
+            
             if (config.AUTO_REPLY === 'true') {
                 //if (isOwner) return;        
                 await m.reply(data[text])
